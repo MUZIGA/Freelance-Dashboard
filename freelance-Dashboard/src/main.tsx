79bfb4912f7789ac0cee464bcd43099f48ed028e
muziga judith <umuzigajudit@gmail.com>
2025-11-03 14:47:18 +0200

    added

--- freelance-Dashboard/src/main.tsx
+++ freelance-Dashboard/src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AppProvider } from "./state/context";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from './Context/AppContext';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
